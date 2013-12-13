10fb0a9c5c21841edf9bfe8a85baa3b7cb71ac12
Ben Schwartz <ben.schwartz@bancvue.com>
2013-12-13 09:32:15 -0600

    updates

--- docker_test.sh
+++ docker_test.sh
@@ -5,5 +5,7 @@ if [ ! -f ./docker-testing-container/docker-latest ]; then
 	wget -O ./docker-testing-container/docker-latest https://get.docker.io/builds/Linux/x86_64/docker-latest
 fi
 
+ln -sf `pwd` ./docker-testing-container/wc
+
 docker build -t dind ./docker-testing-container/
-docker run -privileged -t -i -v `pwd`:/opt dind
+docker run -privileged -t -i dind
